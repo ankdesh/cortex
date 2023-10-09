94540b05a442fbde3ac476d762ab4585c60b6871
ankdesh <a.s.deshwal@gmail.com>
2023-10-09 20:24:34 +0530

    Remove dummy modules

--- cortex/module1.py
+++
@@ -1,9 +0,0 @@
-# module1.py
-
-def function1():
-    """
-    Description of function1.
-    """
-    pass  # Your code here
-
-# Add more functions and classes as needed.
--- cortex/module2.py
+++
@@ -1,9 +0,0 @@
-# module2.py
-
-def function2():
-    """
-    Description of function2.
-    """
-    pass  # Your code here
-
-# Add more functions and classes as needed.
